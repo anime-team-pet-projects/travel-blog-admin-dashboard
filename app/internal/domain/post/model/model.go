db016f4360e9d5bf54d72a1b05da39024131f180
s1veme <kpdpav@gmail.com>
2022-08-10 20:20:23 +0300

    add(app/internal/domain): add field CreatedAt and UpdatedAt in Post struct

--- app/internal/domain/post/model/model.go
+++ app/internal/domain/post/model/model.go
@@ -1,8 +1,12 @@
 package model
 
+import "time"
+
 type Post struct {
-	ID      string `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	OwnerID string `json:"owner_Id"`
+	ID        int        `json:"id"`
+	Title     string     `json:"title"`
+	Content   string     `json:"content"`
+	OwnerID   int        `json:"owner_id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt *time.Time `json:"updated_at"`
 }
